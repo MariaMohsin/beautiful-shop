52fa4dfb2adbd59af8db827daf711ae16993e38c
MariaMohsin <143216809+MariaMohsin@users.noreply.github.com>
2025-07-11 20:25:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from agents import Agent, AsyncOpenAI,OpenAIChatCompletionsModel,Runner
+
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
 # ✅ About route
 @app.get("/", response_class=HTMLResponse)
 async def about(request: Request):
